dc6913b7ecbbcec8d44e37f05152d7f49127f0d9
SaiRevanthSadhu <sairevanthsadhu@loyalistcollege.com>
2024-09-27 09:35:22 -0400

    addint

---
+++ Addint.go
@@ -0,0 +1,10 @@
+package main
+
+func AddInt(a int, b int) int {
+	// var c int
+	// var c int = 2
+	c := 2
+	c = 3
+	c = 6
+	return a + b + c
+}
--- main.go
+++ main.go
@@ -4,4 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(AddInt(1, 2))
 }

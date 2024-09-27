a883841ad30eb67f1158e2f2be1b92ced43ceda5
SaiRevanthSadhu <sairevanthsadhu@loyalistcollege.com>
2024-09-27 12:28:59 -0400

    calc

---
+++ calc.go
@@ -0,0 +1,8 @@
+package main
+
+func calc_n(a, b int) (int, float64) {
+	sum := a + b
+	division := a / b
+
+	return sum, float64(division)
+}
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	greeting()
 	fmt.Println("Hello, World!")
 	fmt.Println(AddInt(5, 6))
+	fmt.Println(calc_n(24, 6))
 }
